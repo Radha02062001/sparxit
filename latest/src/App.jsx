ca767a904697e54fa893739df0e1f456b1d2da56
annie199810 <developerannie057@gmail.com>
2022-11-24 11:51:05 +0530

    update

--- latest/src/App.jsx
+++ latest/src/App.jsx
@@ -1,30 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import Accla from "./component/main/accla/accla"
-import Hero from "./component/main/hero/hero"
-import Motto from './component/main/motto/motto'
-import Stake from './component/main/stake/stake'
-import Web from './component/main/web/web'
-import Block from './component/main/block/block'
-import Class from './component/main/reco/reco'
-import About from "./component/main/about/about"
-import From from "./component/main/from/from"
-import Box from './component/main/box/box'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import Accla from "./component/main/accla/accla";
+import Hero from "./component/main/hero/hero";
+import Motto from "./component/main/motto/motto";
+import Stake from "./component/main/stake/stake";
+import Web from "./component/main/web/web";
+import Block from "./component/main/block/block";
+import Class from "./component/main/reco/reco";
+import About from "./component/main/about/about";
+import From from "./component/main/from/from";
+import Box from "./component/main/box/box";
+import Header from "./component/header/header";
+import Footer from "./component/footer/footer";
 function App() {
-  return(
+  return (
     <>
-    <Hero/>
-    <Accla/>
-    <Motto/>
-    <Stake/>
-    <Web/> 
-    <Block/>
-    <Class/>
-    <About/>
-    <From/>
-    <Box/>
+      <Header />
+      <Hero />
+      <Accla />
+      <Motto />
+      <Stake />
+      <Web />
+      <Block />
+      <Class />
+      <About />
+      <From />
+      <Box />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
