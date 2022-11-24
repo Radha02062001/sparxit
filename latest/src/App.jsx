ba40cdc1baa9d7397ba847f9deb4601c4deeb6bf
Radha02062001 <rajalakshmi96462@gmail.com>
2022-11-24 12:54:05 +0530

    hii

--- latest/src/App.jsx
+++ latest/src/App.jsx
@@ -9,10 +9,12 @@ import Block from './component/main/block/block'
 import Class from './component/main/reco/reco'
 import About from "./component/main/about/about"
 import From from "./component/main/from/from"
-import Box from './component/main/box/box'
+
+import Header from './component/header/header'
 function App() {
   return(
     <>
+    <Header/>
     <Hero/>
     <Accla/>
     <Motto/>
@@ -22,7 +24,7 @@ function App() {
     <Class/>
     <About/>
     <From/>
-    <Box/>
+  
     </>
   )
 }
