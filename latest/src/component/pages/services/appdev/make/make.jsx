56691abe8cb3807d2048025c0a3c92bc917f1177
Radha02062001 <rajalakshmi96462@gmail.com>
2022-11-28 11:34:15 +0530

    update

---
+++ latest/src/component/pages/services/appdev/make/make.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./make.css"
+const Make=()=>{
+
+    return(
+<div className="make">
+<h1>What Makes a Credible Mobile App<br></br> Development Company</h1>
+<div className="credible">
+  <div className="company">
+  <div className="faster">
+    <h2>App Design</h2><hr></hr>
+    <h2>Faster Deployment</h2><hr></hr>
+    <h2>Smart Developers</h2><hr></hr>
+    <h2>Rocksolid Security</h2><hr></hr>
+    <h2>Best Functionality</h2>
+  </div>
+  <div className="smart">
+    <h3>You work with our team closely to develop the custom UI/UX Mobile app design.</h3>
+    <h3>We care about your time, so we develop fast and deploy your app on time.</h3>
+    <h3>Our developers are smart and they can grasp and implement your ideas.</h3>
+    <h3>We implement the best security features to strengthen safety.</h3>
+    <h3>The app usability is at the core of our app design & development process.</h3>
+  </div>
+  </div>
+  <div className="bene">
+    <img src="	https://www.sparxitsolutions.com/assets-style/images/benefits-img.svg" alt=""
+    width="500"
+    height="500" />
+  </div>
+</div>
+</div>
+    )
+}
+export default Make;
