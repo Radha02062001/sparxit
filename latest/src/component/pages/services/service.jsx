49d168891fb951776ed439274b9a5353258fe796
Radha02062001 <rajalakshmi96462@gmail.com>
2022-11-28 13:00:58 +0530

    push

--- latest/src/component/pages/services/service.jsx
+++ latest/src/component/pages/services/service.jsx
@@ -2,12 +2,24 @@ import React from "react";
 import Mobile from "./appdev/mobileapp";
 import Map from "./appdev/map/map";
 import Develop from "./appdev/Development/development";
+import Creat from "./appdev/Creat/creat"
+import Growth from "./appdev/Growth/growth"
+import Steps from "./appdev/Steps/steps"
+import Boost from "./appdev/Boos/boost"
+import Request from "./appdev/Request/request"
+import Make from "./appdev/make/make"
 const Services = () => {
   return (
     <>
       <Mobile />
       <Map />
       <Develop />
+      <Make/>
+    <Creat/>
+    <Growth/>
+    <Steps/> 
+    <Boost/>
+    <Request/>
     </>
   );
 };
